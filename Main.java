f64e8c1b546ed880b88bcc47ef06a2b61149c639
Xuan2007Mel <56696430+xuan24325@users.noreply.github.com>
2020-05-18 12:06:31 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+  public static void main(String[] args){
+    for(int i =9; i>=0; i--){
+      for(int k =i; k>=0; k--){
+        System.out.print(k + " ");
+      }
+      System.out.println();
+    }
+
+  }
+}
