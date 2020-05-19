c046111ed428dc0d986f39c6e3644eec64a63f4d
Xuan2007Mel <56696430+xuan24325@users.noreply.github.com>
2020-05-19 13:25:31 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,3 +9,14 @@ public class Main{
 
   }
 }
+
+
+public class Main{
+  public static void main(String[] args){
+    int i = 9;
+    while(i>=0){
+      int k =i;
+      while (k >=0){
+        System.out.print(k + " ");
+      }
+    }
